2187c171507ab56698fbf40d791ee787ed2cbb4b
Chris Bernt <christopher.bernt@duke.edu>
2016-04-04 23:01:20 -0400

    added more framework elements to simulation, added im history

--- src/Agent.java
+++ src/Agent.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -5,31 +6,67 @@ import java.util.Map;
 
 public class Agent {
 	
-	private List<Agent> myInformationSet;
-	private List<Agent> myInteractionSet;
+	public List<Agent> myInformationSet;
+	public List<Agent> myInteractionSet;
 	
-	private int currentAction;
-	private int currentOutcome;
+	private int myCurrentAction;
+	private double myCurrentOutcome;
+	
+	//private int myBank; TODO this?
 	
 	private List<Integer> myPastActions;
-	private List<Integer> myPastOutcomes;
+	private List<Double> myPastOutcomes;
+	
+	private int myGroup; //TODO
+	
+	private int[][] gameMatrix = new int[][] {{1,0}, 
+			  								  {0,5}};
 	
 	
-	public int chooseNewAction(){
+	private int myId;
+	
+	private static final double DISCOUNT = 0.90; 
+	
+	public Agent(int id){
+		myId = id;
+		myInformationSet = new ArrayList<Agent>();
+		myInteractionSet = new ArrayList<Agent>();
+		myPastActions = new ArrayList<Integer>();
+		myPastOutcomes = new ArrayList<Double>();
+	}
+	
+	public void chooseNewAction(){
+		myCurrentAction = IMHistory();
+	}
+	
+	public void interactWithAllNeighbors(){
+		
+		double total = 0;
+		
+		for(Agent other : myInteractionSet){
+			int utility = this.interactWith(other, gameMatrix);
+			total += utility;
+		}
+		myCurrentOutcome = total / (myInteractionSet.size());
+		
+		myPastActions.add(myCurrentAction);
+		myPastOutcomes.add(myCurrentOutcome);
 		
 	}
 	
 	
-	public int interactWith(Agent other, int[][] gameMatrix){
-		
-		return gameMatrix[currentAction][other.currentAction]; //TODO check indexing, ij or ji 
-		
+	private int interactWith(Agent other, int[][] game){
+		return game[myCurrentAction][other.myCurrentAction]; //TODO check indexing, ij or ji 
 	}
 	
 	private int BR(double threshold){ //TODO only works with coordination game
 		Map<Integer, Integer> neighborsActions = new HashMap<Integer, Integer>();
 		for(Agent a : myInformationSet){
-			int action = a.myPastActions.get(myPastActions.size() - 1);
+			
+			//System.out.println(this + ": " + a + ": " + a.myPastActions);
+			
+			
+			int action = a.myPastActions.get( a.myPastActions.size() - 1);
 			
 			if(!neighborsActions.containsKey(action)){
 				neighborsActions.put(action, 1);
@@ -41,8 +78,10 @@ public class Agent {
 		
 		for(Integer act : neighborsActions.keySet()){
 			
-			double proportion = neighborsActions.get(act) / myInformationSet.size();
-			if(proportion >= threshold) return act;
+			double proportion = (double) neighborsActions.get(act) / (double) myInformationSet.size();
+			if(proportion >= threshold){
+				return act;
+			}
 			
 		}
 		
@@ -52,6 +91,76 @@ public class Agent {
 	}
 	
 	private int IM(){
+		return 0;
+	}
+	
+	
+	
+	private int IMHistory(){
+		Map<Integer, Double> neighborsActions = new HashMap<Integer, Double>();
+		Map<Integer, Double> neighborsActionsCount = new HashMap<Integer, Double>();
+		for(Agent a : myInformationSet){
+			
+			//look at last 5 choices
+			for(int j = 0; j < 5; j++){
+				
+				int action = a.myPastActions.get( a.myPastActions.size() - 1 - j);
+				double result = Math.pow(DISCOUNT, j) *  a.myPastOutcomes.get(a.myPastOutcomes.size() - 1 - j);
+				
+				if(!neighborsActions.containsKey(action)){
+					neighborsActions.put(action, result);
+					neighborsActionsCount.put(action, 1.0);
+				}
+				else{
+					neighborsActions.put(action, neighborsActions.get(action) + result);
+					neighborsActionsCount.put(action, neighborsActionsCount.get(action) + 1.0);
+				}
+				
+			}
+			
+			
+		}
 		
+		//average all
+		for(int action : neighborsActions.keySet()){
+			
+			neighborsActions.put(action, neighborsActions.get(action) / neighborsActionsCount.get(action));
+			
+		}
+		
+		//find best
+		int curBestAction = 0;
+		double curBestOutcome = -1;
+		for(int possibleAction : neighborsActions.keySet()){
+			
+			if(neighborsActions.get(possibleAction) > curBestOutcome){
+				curBestOutcome = neighborsActions.get(possibleAction);
+				curBestAction = possibleAction;
+			}
+			
+		}
+		return curBestAction;
+		
+	}
+	
+	
+	public void addAgentToInfoNetwork(Agent a){
+		this.myInformationSet.add(a);
+	}
+	
+	public void addAgentToInteractionNetwork(Agent a){
+		this.myInteractionSet.add(a);
+	}
+	
+	public String toString(){
+		return Integer.toString(myId);
+	}
+	
+	public void setCurrentAction(int action){
+		myCurrentAction = action;
+	}
+	
+	public int getCurrentAction(){
+		return myCurrentAction;
 	}
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,26 +1,142 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 
 public class Simulation {
 	
-	private int[][] gameMatrix = new int[][] {{1,0}, 
-											  {0,5}};
+
 	
-	private int numSteps;
+	private final static int NUM_STEPS = 500;
+	private List<Agent> myAgentList;
 	
-	
+	private Random r = new Random(1237);
 	
 	
 	public static void main(String[] args){
-		
+		Simulation s = new Simulation();
+		s.setup();
+		s.run();
 	}
 	
-	public List<Agent> createNetwork(){
+	private void setup(){
+		myAgentList = createNetwork("infoCenter.txt", "interCenter.txt");
 		
+		for(Agent a : myAgentList){
+			System.out.println(a.myInformationSet + " --- " + a.myInteractionSet);
+		}
+	}
+	
+	public List<Agent> createNetwork(String informationNetwork, String interactionNetwork){ 
+		
+		List<String[]> adjacencies = readFile(informationNetwork);
+		
+//		for(String[] s : adjacencies){
+//			System.out.println(Arrays.toString(s));
+//		}
+		
+		int numAgents = adjacencies.size();
+		
+		
+		
+		List<Agent> agents = new ArrayList<Agent>();
+		for(int i = 0; i < numAgents; i++){
+			agents.add(new Agent(i));
+		}
+		
+		for(int i = 0; i < adjacencies.size(); i++){
+			for(int j = 0; j < adjacencies.size(); j++){
+				if(adjacencies.get(i)[j].equals("1")){
+					agents.get(i).addAgentToInfoNetwork(agents.get(j));
+				}
+			}
+		}
+		
+		adjacencies = readFile(interactionNetwork);
+		
+		
+		for(int i = 0; i < adjacencies.size(); i++){
+			for(int j = 0; j < adjacencies.size(); j++){
+				if(adjacencies.get(i)[j].equals("1")){
+					agents.get(i).addAgentToInteractionNetwork(agents.get(j));
+				}
+			}
+		}
+
+		return agents;
 	}
 	
 	public void run(){
+		for(int i = 0; i < NUM_STEPS; i++){
+			
+			if(i < 5){ //randomly assign at first, TODO history data
+				
+				for(Agent a : myAgentList){
+					int action = r.nextInt(2); //random int either 0 or 1
+					//if(i == 0) System.out.println(action);
+					a.setCurrentAction(action);
+					
+				}
+				
+			}
+			
+			
+			for(Agent a : myAgentList){
+				a.interactWithAllNeighbors();
+			}
+			
+			
+			
+//			Agent luckyAgent = myAgentList.get(r.nextInt(myAgentList.size()));
+//			luckyAgent.chooseNewAction();
+			
+			if(i >= 5){
+				for(Agent a : myAgentList){
+					a.chooseNewAction();
+				}
+			}
+			
+			
+		}
 		
+		//end state
+		for(Agent a : myAgentList){
+			System.out.println(a + ": " + a.getCurrentAction());
+		}
+		
+	}
+	
+	private void step(){
+		
+	}
+	
+	private List<String[]> readFile(String filename){
+		Path filePath = Paths.get(filename);
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(filePath);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		List<String[]> adjacencies = new ArrayList<String[]>();
+		while (scanner.hasNextLine()) {
+		    if (scanner.hasNextLine()) {
+		    	//System.out.print("A");
+		    	//System.out.println(scanner.next());
+		    	adjacencies.add(scanner.nextLine().split(" "));
+		        
+		    } else {
+		        scanner.next();
+		    }
+		}
+		scanner.close();
+		return adjacencies;
 	}
 	
 	
